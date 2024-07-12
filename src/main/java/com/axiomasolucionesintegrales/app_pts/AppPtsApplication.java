d68ff85fc0f0c89daf8b417d1afbbe021f144aa4
RafaelTalavera <rtalavera@live.com.ar>
2024-07-12 11:42:00 -0300

    firts commit dependencis

---
+++ src/main/java/com/axiomasolucionesintegrales/app_pts/AppPtsApplication.java
@@ -0,0 +1,13 @@
+package com.axiomasolucionesintegrales.app_pts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axiomasolucionesintegrales/app_pts/AppPtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axiomasolucionesintegrales.app_pts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
